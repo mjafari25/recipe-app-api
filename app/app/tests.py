5fbef4dbb1559e890d87aefe12ebf549b86c358b
mohammadali <mohammad.jafari19961@gmail.com>
2025-09-15 00:52:05 +0330

    Configure Data base.

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+""" 
+Simple tests 
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """ Test th calc module. """
+    
+    def test_add_numbers(self):
+        """ Test adding numbers together. """
+        res = calc.add(5, 6)
+        
+        self.assertEquals(res, 11)
+        
+    def test_subtract_numbers(self):
+        """Test Subtracting numbers. """
+        res = calc.subtract(10, 15)
+        
+        self.assertEqual(res, 5)
